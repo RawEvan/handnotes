02ba31036e12c57ffc4f0f886ac3fc674c7c8f97
Evan <909798432@qq.com>
2018-02-23 03:43:18 +0800

    init

---
+++ src/components/Notes.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="notes">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notes',
+  data () {
+    return {
+      msg: 'Welcome to handnotes'
+    }
+  }
+}
+</script>
